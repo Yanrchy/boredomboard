c0ece653c71af9c1892b8720c31ccccb82ad6b74
Ian Red <68849067+Yanrchy@users.noreply.github.com>
2024-06-10 14:42:41 +0800

    added lobby component

--- projects/app-client/src/app/app.component.ts
+++ projects/app-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranscriptComponent } from './transcript/transcript.component';
+import { LobbyComponent } from './lobby/lobby.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ TranscriptComponent ],
+  imports: [ TranscriptComponent, LobbyComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
