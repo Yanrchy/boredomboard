8b1d0aef77b93dc5519d554b9111794778d61869
Ian Red <68849067+Yanrchy@users.noreply.github.com>
2024-06-10 14:47:43 +0800

    added input field component

--- projects/app-client/src/app/app.component.ts
+++ projects/app-client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranscriptComponent } from './transcript/transcript.component';
 import { LobbyComponent } from './lobby/lobby.component';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ TranscriptComponent, LobbyComponent ],
+  imports: [ TranscriptComponent, LobbyComponent, InputFieldComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
