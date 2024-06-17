983b139ab4f89a8381c838d8273f9ec221b1175f
Ian Red <68849067+Yanrchy@users.noreply.github.com>
2024-06-17 18:32:11 +0800

    added routing

--- projects/app-client/src/app/app.component.ts
+++ projects/app-client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
-import { CommChannelComponent } from './comm-channel/comm-channel.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CommChannelComponent ],
+  imports: [ RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ projects/app-client/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { CommChannelComponent } from "./comm-channel/comm-channel.component";
+
+export const routes = [
+    { path:"comm-channel", component: CommChannelComponent }
+];
