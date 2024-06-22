7fc6afacc7b6db9f6f70a4963a451ff445b013ed
Ian Red <68849067+Yanrchy@users.noreply.github.com>
2024-06-22 17:46:58 +0800

    added connection form

--- projects/app-client/src/app/connection-prompt/connection-prompt.component.ts
+++ projects/app-client/src/app/connection-prompt/connection-prompt.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-connection-prompt',
   standalone: true,
-  imports: [ RouterLink ],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './connection-prompt.component.html',
   styleUrl: './connection-prompt.component.scss'
 })
 export class ConnectionPromptComponent {
+
+  userForm = new FormGroup({
+    username  : new FormControl(''),
+    userColor : new FormControl('')
+  });
+
+  Connect() {
+
+    console.warn(this.userForm.value);
+
+  }
 
 }
