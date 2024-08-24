371e811992d5c22bf60f28fd0e37b55f425b3a4f
Ian Red <68849067+Yanrchy@users.noreply.github.com>
2024-08-24 11:01:51 +0800

    websocket connection between client and server

--- projects/app-client/src/app/channel.service.ts
+++ projects/app-client/src/app/channel.service.ts
@@ -1,24 +1,43 @@
 import { Injectable } from '@angular/core';
 import { UserProfileService } from './user-profile.service';
 
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ChannelService {
 
   m_ProfileService: UserProfileService;
-  m_Messages: Message[];
+  m_Messages:       Message[];
+  m_Socket!:        WebSocketSubject<unknown>;
 
   constructor(profileService: UserProfileService) {
 
     this.m_ProfileService = profileService;
     this.m_Messages = new Array<Message>();
 
+    this.Connect("ws://localhost:8000"); //TODO: this should not be hard coded, use .env or database
+
+    this.m_Socket.subscribe(this.OnMessageReceived.bind(this));
+
    }
+
+   OnMessageReceived<T>(message: T | any): void {
+
+    this.m_Messages.push(new Message(message.sender, message.color, message.text));
+    
+  }
 
    Send(text: string): void {
 
     this.m_Messages.push(new Message(this.m_ProfileService.m_Username, this.m_ProfileService.m_Color, text));
+
+   }
+
+   Connect(url: string): void {
+
+    this.m_Socket = webSocket(url);
 
    }
 
